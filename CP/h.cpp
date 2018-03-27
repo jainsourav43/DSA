35bde165161853d8a57f3f480498c2fd2080aa6d
sourav <jainsourav43@gmail.com>
2018-03-27 12:52:16 +0530

    First Commit

--- CP/h.cpp
+++ CP/h.cpp
@@ -1,21 +1,12 @@
 #include<iostream>
 using namespace std;
-#define ll long long
-ll fact(ll n)
+#include<cmath>
+void my(int a)
 {
-	ll i,j=1;
-	for(i=2;i<=n;i++)
-	j=j*i;
-	return j;
+	cout<<a<<endl;
+	
 }
 int main()
 {
-ll n,m,c;
-ll i,j,k;
-cin>>n>>m>>c;
-i=n-c;
-j=m-c;
-k=i+j+c;
-cout<<fact(k-1)<<endl;
-return 0;
+	my(pow(2,2));
 }
